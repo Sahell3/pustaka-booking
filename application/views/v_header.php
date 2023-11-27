a0a55a9e91804c1226b11896fdc7f4995e5abc11
Sahell3 <dedpolllll9@gmail.com>
2023-11-27 14:10:00 +0700

    pustaka

--- application/views/v_header.php
+++ application/views/v_header.php
@@ -13,8 +13,7 @@
         <header>
             <hgroup>
                 <h1>RentalBuku.net</h1>
-                <h3>Membuat Template Sederhana dengan
-                    CodeIgniter</h3>
+                <h3>Membuat Template Sederhana dengan CodeIgniter</h3>
             </hgroup>
             <nav>
                 <ul>
